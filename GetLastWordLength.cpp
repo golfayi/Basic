83eefbc867391b5cd6dfc3f443bfb416cb70e637
golfayi <golfayi@sina.com>
2018-09-05 20:17:20 +0800

    计算字符串最后一个单词的长度，单词以空格隔开。

---
+++ GetLastWordLength.cpp
@@ -0,0 +1,23 @@
+//计算字符串最后一个单词的长度，单词以空格隔开。
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string str;
+    getline(cin, str);
+    int size = str.length();
+    int i;
+    int count = 0;
+
+    for(i = size; i >=0; i--)
+    {
+        if(i == 0 || str[i - 1] == ' ')
+            break;
+        count ++;
+    }
+    cout << count << endl;
+    return 0;
+}
